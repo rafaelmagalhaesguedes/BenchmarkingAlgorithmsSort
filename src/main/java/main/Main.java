a7133850681117530fb23c7145c1a94bc9f57b64
rafael <rafaelmagalhaes1@live.com>
2024-06-03 13:41:45 -0300

    refactor: main class

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,35 +1,80 @@
 package main;
 
-import Algorithms.BubbleSort;
+import core.SortService;
+import java.util.List;
+import java.util.Scanner;
 
+/**
+ * The type Main.
+ */
 public class Main {
 
-  // Main
+  /**
+   * The entry point of application.
+   *
+   * @param args the input arguments
+   */
   public static void main(String[] args) {
+    SortService sort = new SortService();
+    Scanner scanner = new Scanner(System.in);
+    boolean exit = false;
 
-    // Declares an array of integers
-    int[] array = { 10, 5, 3, 1, 20, 30 };
+    while (!exit) {
+      System.out.print("\n+================= Benchmarking Algorithms Sort ===================+");
+      System.out.print("\n\n1. Run Benchmarking");
+      System.out.print("\n2. Exit");
+      System.out.print("\n\nChoose an option: ");
 
-    // Bubble Sort
-    bubble(array);
-  }
+      int choice = 0;
+      boolean validChoice = false;
 
-  public static void bubble(int[] array) {
-    System.out.print("\n+============= Bubble Sort Algorithm =============+");
-    System.out.print("\n\nOriginal Array: ");
-    for (int n : array) {
-      System.out.print(n + " ");
+      while (!validChoice) {
+        try {
+          choice = scanner.nextInt();
+          validChoice = true;
+        } catch (Exception e) {
+          System.out.println("Invalid input. Please enter an integer.");
+          scanner.next(); // Clear the invalid input
+        }
+      }
+
+      switch (choice) {
+        case 1:
+          int total = 0;
+          boolean validInput = false;
+
+          while (!validInput) {
+            System.out.print("\nEnter total items: ");
+            try {
+              total = scanner.nextInt();
+              validInput = true;
+            } catch (Exception e) {
+              System.out.println("Invalid input. Please enter an integer.");
+              scanner.next(); // Clear the invalid input
+            }
+          }
+
+          List<String> items = sort.readExcelData(total);
+          String[] itemsArray = items.toArray(new String[0]);
+
+          sort.bubbleSortString(itemsArray);
+          sort.mergeSortStrings(itemsArray);
+
+          System.out.println("\n\nPress enter to back to the menu...");
+          scanner.nextLine();
+          scanner.nextLine();
+          break;
+
+        case 2:
+          exit = true;
+          System.out.println("Exiting the program.");
+          break;
+
+        default:
+          System.out.println("Invalid choice. Please enter 1 or 2.");
+          break;
+      }
     }
-
-    // Call bubble sort to sort the array
-    BubbleSort.sort(array);
-
-    // Display ordered array
-    System.out.print("\nOrdered array: ");
-    for(int num : array) {
-      System.out.print(num + " ");
-    }
-
-    System.out.println("\n");
+    scanner.close();
   }
 }
